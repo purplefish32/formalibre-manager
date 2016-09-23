81247327baa62aeec0214aad5b869eea2118faef
nlko <nospam1@thomasson.fr>
2016-09-23 23:09:22 +0200

    Frontend / Create layout

--- files/frontend_angular2/app/app.component.ts
+++ files/frontend_angular2/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+    templateUrl: './templates/app.component.html'
 })
-export class AppComponent { }
+
+export class AppComponent {
+}
