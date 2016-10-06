02c791c3cfd6b7e250790e059547021e42ad3db7
Donovan Tengblad <purplefish32@gmail.com>
2016-10-06 11:22:27 +0200

    Change API URL do an environment variable

--- files/frontend/js/app.js
+++ files/frontend/js/app.js
@@ -1,11 +1,8 @@
-protocol ="http://"
-base_url = "manager.loc/"
-
 angular.
   module("managerApp", [
     'angular-loading-bar',
     'Router'
   ]).
   value('Config', {
-    api_url:protocol+"api."+base_url
+    api_url:process.env.API_URL + "/"
   })
