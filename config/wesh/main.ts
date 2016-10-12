f05366e591b48eb8f9c9f09aec36571d79a5cbff
nlko <nospam1@thomasson.fr>
2016-10-12 09:48:24 +0200

    Config / Generate platform html from config

--- config/wesh/main.ts
+++ config/wesh/main.ts
@@ -2,6 +2,7 @@ let pretty = true
 
 import * as config from '../../model.json'
 import * as fl_servers from './fl_servers'
+import * as fl_platforms from './fl_platforms'
 import {fl_container, fl_element, fl_jadeRenderer} from './fl_comp'
 
 interface String {
@@ -12,3 +13,4 @@ interface String {
 let renderer = new fl_jadeRenderer(pretty, false)
 
 fl_servers.doJob(config,renderer)
+fl_platforms.doJob(config,renderer)
