12f3a0b242c84b59a704626cde7854dea443fab7
Nicolas THOMASSON <nospam1@thomasson.fr>
2016-10-13 16:50:18 +0200

    Config / Generate client html from config

--- config/wesh/main.ts
+++ config/wesh/main.ts
@@ -3,6 +3,7 @@ let pretty = true
 import * as config from '../../model.json'
 import * as fl_servers from './fl_servers'
 import * as fl_platforms from './fl_platforms'
+import * as fl_clients from './fl_clients'
 import {fl_container, fl_element, fl_jadeRenderer} from './fl_comp'
 
 interface String {
@@ -14,3 +15,4 @@ let renderer = new fl_jadeRenderer(pretty, false)
 
 fl_servers.doJob(config,renderer)
 fl_platforms.doJob(config,renderer)
+fl_clients.doJob(config,renderer)
